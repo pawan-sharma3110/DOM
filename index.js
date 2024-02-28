74aa2c074d6edf33daae2101ee780869fa021f00
Pawan Sharma <pawansharma93520@gmail.com>
2024-02-28 22:16:51 +0530

    changing content and atributes by DOM

---
+++ index.js
@@ -0,0 +1,10 @@
+alert('Hello World')
+var heading = document.querySelector('h1')
+document.querySelector('h1').innerHTML = 'Hello World'
+heading.style.color = 'red'
+document.querySelector('li a').style.color = 'red'
+
+document.querySelector('button').style.backgroundColor = 'yellow'
+document.querySelector('h1').classList.add('size')
+document.querySelector('h1').innerHTML = '<em>Hey</em>'
+document.querySelector('a').setAttribute('href', 'https://www.flipkart.com');
